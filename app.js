2e280475575515b0527de0ba2502c08e7f5ed2d2
axel castro <castroaxel.f@gmail.com>
2023-05-09 12:16:59 +0200

    ajout route delete

--- app.js
+++ app.js
@@ -29,6 +29,9 @@ app.post("/api/users",userHandlers.postUser);
 app.put("/api/movies/:id", movieHandlers.updateMovie);
 app.put("/api/users/:id", userHandlers.updateUser);
 
+app.delete("/api/movies/:id", movieHandlers.deleteMovie);
+app.delete("/api/users/:id", userHandlers.deleteUser);
+
 app.listen(port, (err) => {
   if (err) {
     console.error("Something bad happened");
