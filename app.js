8b94a79a6122be763eff9affb2bd0604ca8e35fb
Axel C <118440119+xFokko@users.noreply.github.com>
2023-06-12 11:30:46 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const { hashPassword } = require("./auth.js");
 
 const express = require("express");
 
@@ -6,7 +7,7 @@ const app = express();
 
 app.use(express.json());
 
-const port = process.env.PORT ?? 5000;
+const port = process.env.APP_PORT ?? 5000;
 
 const welcome = (req, res) => {
   res.send("Welcome to my favourite movie list");
@@ -15,21 +16,19 @@ const welcome = (req, res) => {
 app.get("/", welcome);
 
 const movieHandlers = require("./movieHandlers");
-const userHandlers = require("./userHandlers");
 
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
-app.get("/api/users",userHandlers.getUsers);
-app.get("/api/users/:id",userHandlers.getUserById);
-
-
 app.post("/api/movies", movieHandlers.postMovie);
-app.post("/api/users",userHandlers.postUser);
-
 app.put("/api/movies/:id", movieHandlers.updateMovie);
-app.put("/api/users/:id", userHandlers.updateUser);
-
 app.delete("/api/movies/:id", movieHandlers.deleteMovie);
+
+const userHandlers = require("./userHandlers");
+
+app.get("/api/users", userHandlers.getUsers);
+app.get("/api/users/:id", userHandlers.getUserById);
+app.post("/api/users", hashPassword, userHandlers.postUser);
+app.put("/api/users/:id", userHandlers.updateUser);
 app.delete("/api/users/:id", userHandlers.deleteUser);
 
 app.listen(port, (err) => {
